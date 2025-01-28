81b8307a98064b71ccc18d2c6d5deb01c4135d02
TheUknownProgrammer <iidcblazob@gmail.com>
2025-01-29 01:15:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+const calculationArea = document.getElementById("calculation");
+var historyCalculation = [];
+var calculated = false;
+var previousAnswerVal;
+
+const getLastChar = (str) => str[str.length-1];
+
+function append(char) {
+    if(calculated) {
+        calculationArea.innerHTML = "";
+        calculated = false;
+    }
+
+    if(isNaN(char)) { // if not a number
+        if(!isNaN(getLastChar(calculationArea.innerHTML))) { // if the last character is considered a number
+            calculationArea.innerHTML += char;
+        } else {
+            if(calculationArea.innerHTML != "") {
+            calculationArea.innerHTML = calculationArea.innerHTML.substring(0,calculationArea.innerHTML.length-1) + char;
+            }
+        }
+    } else { // if a number is inserted to the calculator
+        calculationArea.innerHTML += char;
+    }
+
+    calculationArea.scrollTo(calculationArea.scrollWidth,0)
+}
+
+function calculate() {
+    // add a check here if the innerHTML of calculatorArea have at least one operator of those +,-,*,/.
+    if(!calculated) {
+        try {
+            let result = eval(calculationArea.innerHTML);
+            calculationArea.innerHTML = eval(result);
+            historyCalculation.push(result);
+            previousAnswerVal = result;
+        } catch(err) {
+            calculationArea.innerHTML = err.message;
+        }
+    }
+    calculated = true;
+}
+
+function clearCalculation() {
+    calculationArea.innerHTML = "";
+}
+
+function removeCharacter() {
+    if(!calculated) {
+    calculationArea.innerHTML = calculationArea.innerHTML.substring(0,calculationArea.innerHTML.length-1);
+    }
+}
+
+function previousAnswer() {
+    if(previousAnswerVal != undefined && !calculated) {
+        if(!isNaN(getLastChar(calculationArea.innerHTML))) {
+        calculationArea.innerHTML += "*" + previousAnswerVal;
+        } else {
+            calculationArea.innerHTML += previousAnswerVal;
+        }
+    }
+}
+
+function addDot() {
+    var newStr = calculationArea.innerHTML.replaceAll(/[*/+-]/gi,",");
+    var newArr = newStr.split(",");
+    var lastNumber = newArr[newArr.length-1];
+
+    if(!lastNumber.includes(".") && calculationArea.innerHTML != "" && !calculated) {
+        calculationArea.innerHTML += ".";
+    }
+}
